7d63a18dad454484e7cf28e06af7024f48ddbc0f
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2020-07-12 22:13:50 -0500

    files

--- include/darknet.h
+++ include/darknet.h
@@ -305,9 +305,6 @@ struct layer {
     int embedding_size;
     float sim_thresh;
     int track_history_size;
-    int dets_for_track;
-    int dets_for_show;
-    float track_ciou_norm;
     int coords;
     int background;
     int rescore;
@@ -1071,7 +1068,7 @@ void stop_timer_and_show();
 void stop_timer_and_show_name(char *name);
 void show_total_time();
 
-void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim_thresh, float track_ciou_norm, int deque_size, int dets_for_track, int dets_for_show);
+void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim_thresh, int deque_size);
 int fill_remaining_id(detection *new_dets, int new_dets_num, int new_track_id, float thresh);
 
 
--- src/http_stream.cpp
+++ src/http_stream.cpp
@@ -775,28 +775,13 @@ int check_prob(detection det, float thresh)
     return 0;
 }
 
-int check_classes_id(detection det1, detection det2, float thresh)
-{
-    if (det1.classes != det2.classes) {
-        printf(" Error: det1.classes != det2.classes \n");
-        getchar();
-    }
-
-    for (int i = 0; i < det1.classes; ++i) {
-        if (det1.prob[i] > thresh && det2.prob[i] > thresh) return 1;
-    }
-    return 0;
-}
-
-int fill_remaining_id(detection *new_dets, int new_dets_num, int new_track_id, float thresh, int detection_count)
+int fill_remaining_id(detection *new_dets, int new_dets_num, int new_track_id, float thresh)
 {
     for (int i = 0; i < new_dets_num; ++i) {
         if (new_dets[i].track_id == 0 && check_prob(new_dets[i], thresh)) {
             //printf(" old_tid = %d, new_tid = %d, sim = %f \n", new_dets[i].track_id, new_track_id, new_dets[i].sim);
-            if (new_dets[i].sort_class > detection_count) {
-                new_dets[i].track_id = new_track_id;
-                new_track_id++;
-            }
+            new_dets[i].track_id = new_track_id;
+            new_track_id++;
         }
     }
     return new_track_id;
@@ -810,8 +795,7 @@ float *make_float_array(float* src, size_t size)
 }
 
 struct detection_t : detection {
-    int det_count;
-    detection_t(detection det) : detection(det), det_count(0)
+    detection_t(detection det) : detection(det)
     {
         if (embeddings) embeddings = make_float_array(det.embeddings, embedding_size);
         if (prob) prob = make_float_array(det.prob, classes);
@@ -834,7 +818,7 @@ struct detection_t : detection {
 
 
 
-void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim_thresh, float track_ciou_norm, int deque_size, int dets_for_track, int dets_for_show)
+void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim_thresh, int deque_size)
 {
     static int new_track_id = 1;
     static std::deque<std::vector<detection_t>> old_dets_dq;
@@ -862,7 +846,7 @@ void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim
 
     // sort similarity
     std::sort(sim_det.begin(), sim_det.end(), [](similarity_detections_t v1, similarity_detections_t v2) { return v1.sim > v2.sim; });
-    //if(sim_det.size() > 0) printf(" sim_det_first = %f, sim_det_end = %f \n", sim_det.begin()->sim, sim_det.rbegin()->sim);
+    if(sim_det.size() > 0) printf(" sim_det_first = %f, sim_det_end = %f \n", sim_det.begin()->sim, sim_det.rbegin()->sim);
 
 
     std::vector<int> new_idx(new_dets_num, 1);
@@ -874,46 +858,26 @@ void set_track_id(detection *new_dets, int new_dets_num, float thresh, float sim
         const int new_id = sim_det[index].new_id;
         const int old_id = sim_det[index].old_id;
         const int track_id = old_dets[old_id].track_id;
-        const int det_count = old_dets[old_id].sort_class;
-        //printf(" ciou = %f \n", box_ciou(new_dets[new_id].bbox, old_dets[old_id].bbox));
-        if (track_idx[track_id] && new_idx[new_id] && old_idx[old_id] && check_classes_id(new_dets[new_id], old_dets[old_id], thresh)) {
-            float sim = sim_det[index].sim;
-            float ciou = box_ciou(new_dets[new_id].bbox, old_dets[old_id].bbox);
-            sim = sim * (1 - track_ciou_norm) + ciou * track_ciou_norm;
-            if (sim_thresh < sim) {
-                new_dets[new_id].sim = sim;
-                new_dets[new_id].track_id = track_id;
-                new_dets[new_id].sort_class = det_count + 1;
-                new_idx[new_id] = 0;
-                old_idx[old_id] = 0;
-                if(track_id) track_idx[track_id] = 0;
-            }
+        if (check_prob(new_dets[new_id], thresh) && track_idx[track_id] && new_idx[new_id] && old_idx[old_id] && sim_thresh < sim_det[index].sim) {
+            new_dets[new_id].sim = sim_det[index].sim;
+            new_dets[new_id].track_id = track_id;
+            new_idx[new_id] = 0;
+            old_idx[old_id] = 0;
+            track_idx[track_id] = 0;
         }
     }
 
     // set new track_id
-    new_track_id = fill_remaining_id(new_dets, new_dets_num, new_track_id, thresh, dets_for_track);
+    new_track_id = fill_remaining_id(new_dets, new_dets_num, new_track_id, thresh);
 
     // store new_detections to the queue of vectors
     std::vector<detection_t> new_det_vec;
     for (int i = 0; i < new_dets_num; ++i) {
-        if (check_prob(new_dets[i], thresh)) {
+        if(check_prob(new_dets[i], thresh))
             new_det_vec.push_back(new_dets[i]);
-        }
     }
 
-    // add new
-    old_dets_dq.push_back(new_det_vec);
-    // remove old
-    if (old_dets_dq.size() > deque_size) old_dets_dq.pop_front();
-
-    // remove detection which were detected only on few frames
-    for (int i = 0; i < new_dets_num; ++i) {
-        if (new_dets[i].sort_class < dets_for_show) {
-            for (int j = 0; j < new_dets[i].classes; ++j) {
-                new_dets[i].prob[j] = 0;
-            }
-        }
-    }
+    old_dets_dq.push_back(new_det_vec); // add new
+    if (old_dets_dq.size() > deque_size) old_dets_dq.pop_front();   // remove old
 }
 
